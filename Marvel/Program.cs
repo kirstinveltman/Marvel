cb9df499b70b2ab697108a321cf7d99a7808968a
Kirstin Veltman <kirstin@veltman.co>
2018-07-10 13:56:46 -0700

    Fixed stuff

--- Marvel/Program.cs
+++ Marvel/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 
 namespace Marvel
 {
@@ -33,19 +37,45 @@ namespace Marvel
             // get specific data from the Excel spreadsheet and print them to console
             for (int i = 1; i <= rows; i++)
             {
-                string firstName = Convert.ToString((excelRange.Cells[i, 2] as Range).Text);
-                string lastName = Convert.ToString((excelRange.Cells[i, 3] as Range).Text);
-                string color = Convert.ToString((excelRange.Cells[i, 5] as Range).Text);
+                string firstName = Convert.ToString((excelRange.Cells[i, 1] as Range).Text);
+                string lastName = Convert.ToString((excelRange.Cells[i, 2] as Range).Text);
+                int age = Convert.ToInt32((excelRange.Cells[i, 3] as Range).Text);
+                string color = Convert.ToString((excelRange.Cells[i, 4] as Range).Text);
 
-                Console.WriteLine(firstName + " " + lastName + "'s favorite color is: " + color);
+                // connection string and insert string query to db
+                var connectionString = @"Data Source=RAVN\SQLEXPRESS;Initial Catalog=characters;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                string queryString = @"INSERT INTO charactersMarvel (FirstName, LastName, Age, FavoriteColor)
+                                    VALUES (@FirstName, @LastName, @Age, @FavoriteColor)";
+
+                // set up and execute data from each line above and insert into db
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                    command.Parameters.Add("@Age", SqlDbType.Int);
+                    command.Parameters.Add("@FavoriteColor", SqlDbType.VarChar);
+
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@Age"].Value = age;
+                    command.Parameters["@FavoriteColor"].Value = color;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
             }
 
             // exiting Excel application and releasing the COM (which allows binary communication across machine boundaries)
             excelApp.Quit();
             System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
 
+            Console.WriteLine("Excel File Transfer Successful");
             Console.ReadLine();
 
         }
+
     }
 }
